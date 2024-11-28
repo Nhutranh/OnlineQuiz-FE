ec3a8a753309c46ee2c6eeb23f929b997a5dff1a
Duyên Ng <huongduyen.work@gmail.com>
2024-11-28 12:58:04 +0700

    update: add edit-avatar-button in component "UserInfo"

--- src/pages/user/account/UserInfo.jsx
+++ src/pages/user/account/UserInfo.jsx
@@ -1,48 +1,63 @@
-import { useUserStore } from '~/store';
-import images from '~/assets/images';
+import { useUserStore } from "~/store";
+import images from "~/assets/images";
+import { default as Icons } from "~/assets/icons";
 
 const RoleName = {
-  ['admin']: 'Quản lý',
-  ['student']: 'Học viên',
+  ["admin"]: "Quản lý",
+  ["student"]: "Học viên",
 };
 
 const UserInfo = () => {
-  console.log('Loaded AccountInfo');
+  console.log("Loaded AccountInfo");
 
   const user = useUserStore((state) => state.user);
 
   return (
-    <div className="w-full h-screen">
-      <div className="mx-auto max-w-full max-h-[80%] bg-white rounded">
+    <div className="h-screen w-full">
+      <div className="mx-auto max-h-[80%] max-w-full rounded bg-white">
         <div className="justify-items-center space-y-6 py-8 text-base leading-7 text-gray-600">
-          <div className="relative p-1 border border-primary rounded-full">
-            <div className="rounded-xl overflow-hidden w-[32px] h-[32px]  md:h-[52px] md:w-[52px]">
-              <img src={user?.avatar || images.logo} alt="avatar" className="object-cover" />
+          <div className="relative max-w-max">
+            <div className="relative rounded-full border p-1 ring-4 ring-primary drop-shadow-lg">
+              <div className="h-[48px] w-[48px] overflow-hidden rounded-xl p-2 md:h-[72px] md:w-[72px]">
+                <img
+                  src={user?.avatar || images.logo}
+                  alt="avatar"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+            <div className="absolute -bottom-1 -right-1 max-w-min cursor-pointer rounded-full border-2 border-primary bg-white p-1 text-primary hover:bg-primary hover:text-white">
+              <Icons.Pencil />
             </div>
           </div>
-          <p className="text-center text-primary font-extrabold text-xl md:text-2xl">
+
+          <p className="text-center text-xl font-extrabold text-primary md:text-2xl">
             {user.fullName}
           </p>
-          <div className=" text-center text-white font-bold  rounded bg-primary p-2">
-            {user ? RoleName[user.role] : '--'}
+          <div className="rounded bg-primary p-2 text-center font-bold text-white">
+            {user ? RoleName[user.role] : "--"}
           </div>
           {/* <!-- Stats --> */}
-          <div className="divide-y divide-slate-200 px-4 min-w-max">
-            <div className=" text-center font-semibold grid md:grid-flow-col md:grid-cols-2  w-full min-w-full max-w-max flex-wrap gap-4  md:place-content-around md:py-4 overflow-hidden">
-              <div className="min-w-min justify-items-center rounded bg-white border-primary border-solid border-2 p-3 space-y-2">
+          <div className="min-w-max divide-y divide-slate-200 px-4">
+            <div className="grid w-full min-w-full max-w-max flex-wrap gap-4 overflow-hidden text-center font-semibold md:grid-flow-col md:grid-cols-2 md:place-content-around md:py-4">
+              <div className="min-w-min justify-items-center space-y-2 rounded border-2 border-solid border-primary bg-white p-3">
                 <p className="truncate">Số bài tập đã hoàn thành</p>
-                <p className="font-bold text-xl md:text-2xl">{user.numberOfCompleted ?? 0}</p>
+                <p className="text-xl font-bold md:text-2xl">
+                  {user.numberOfCompleted ?? 0}
+                </p>
               </div>
-              <div className="min-w-min justify-items-center rounded bg-white border-primary border-solid border-2 p-3 space-y-2">
+              <div className="min-w-min justify-items-center space-y-2 rounded border-2 border-solid border-primary bg-white p-3">
                 <p className="truncate">Số bài đăng</p>
-                <p className="font-bold text-xl md:text-2xl">{user.numberOfPost ?? 0}</p>
+                <p className="text-xl font-bold md:text-2xl">
+                  {user.numberOfPost ?? 0}
+                </p>
               </div>
             </div>
             {/* <!-- Other info --> */}
-            <div className=" py-4 min-w-full">
-              <div className=" w-full grid grid-flow-col md:grid-cols-2 flex-wrap p-4 gap-2 md:gap-4 ">
-                <div className="font-semibold ">Email</div>
-                <div className=" truncate">{user?.email ?? '---'}</div>
+            <div className="min-w-full py-4">
+              <div className="grid w-full grid-flow-col flex-wrap gap-2 p-4 md:grid-cols-2 md:gap-4">
+                <div className="font-semibold">Email</div>
+                <div className="truncate">{user?.email ?? "---"}</div>
               </div>
             </div>
           </div>
